8faf1888677132bbe9770a94bf14ce67f6eecd58
TomaszMisiowski <156847518+TomaszMisiowski@users.noreply.github.com>
2024-05-05 11:20:00 +0200

    poszło

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-
+let timeoutId;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeBackgroudColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timeoutId = setTimeout(changeBackgroudColor, 1000);
+  return timeoutId;
+}
+const startBtn = document.querySelector('[data-start]');
+startBtn.addEventListener('click', function () {
+  this.disabled = true;
+  stopBtn.disabled = false;
+  changeBackgroudColor();
+});
+console.log(startBtn);
+const stopBtn = document.querySelector('[data-stop]');
+stopBtn.addEventListener('click', function () {
+  this.disabled = true;
+  startBtn.disabled = false;
+  clearTimeout(timeoutId);
+});
